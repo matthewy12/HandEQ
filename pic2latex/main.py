7c808fea1bda470d734c2add7d0111b0c45a32a6
Matthew Yang <msy35@cornell.edu>
2023-03-16 21:16:22 -0400

    main setup

---
+++ pic2latex/main.py
@@ -0,0 +1,14 @@
+""""
+Reads the image file, calls functions in preprocessing module,
+loads the model and does predictions, calls functions in post-processing module
+for output txt file.
+"""
+
+
+def main():
+    print("running as a script")
+
+
+# execute code when the file runs as a script, but not when it’s imported as a module
+if __name__ == '__main__':
+    main()
